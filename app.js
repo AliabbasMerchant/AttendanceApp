6ddf59bd3619f22c25d6304a0cb3caee0e9c16cf
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-10-30 21:29:08 +0530

    Public folder, Flash Mesages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // importing modules
 const express = require('express');
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
@@ -10,7 +11,9 @@ const cookieParser = require('cookie-parser');
 require('./config/passport')(passport);
 
 // setting up mongoose
-mongoose.connect(process.env.MONGO_CONNECTION_STRING, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true, dbName: 'testDB', useUnifiedTopology: true
+});
 
 // setting up the app
 const app = express();
@@ -29,17 +32,19 @@ app.use(
     })
 );
 
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
 // global variables
 app.use((req, res, next) => {
     res.locals.user = req.user;
+    res.locals.success_msgs = req.flash('success_msgs');
+    res.locals.error_msgs = req.flash('error_msgs');
     next();
 });
 
-// routes
-app.use('/', require('routes'));
+app.use('/', require('./routes'));
 
 // starting server
 app.listen(process.env.PORT, process.env.IP);
--- routes.js
+++ routes.js
@@ -1,12 +1,52 @@
 const express = require('express');
-const constants = require('./config/constants');
+const auth = require('./config/auth');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('HELLO');
+router.get('/', (_req, res) => {
+    res.redirect('/login');
 });
 
+router.get('/login', (_req, res) => {
+    res.send('LOGIN PAGE');
+});
+
+router.post('/login', (req, res) => {
+    // TODO
+});
+
+router.get('/register', auth.isAdminLoggedIn, (req, res) => {
+    res.send('REGISTER ADMIN PAGE');
+});
+
+router.post('/register', auth.isAdminLoggedIn, (req, res) => {
+    // TODO
+});
+
+router.get('/home', auth.isAdminLoggedIn, (req, res) => {
+    /*
+    Links to:
+        Users page (View, add, edit)
+        Attendace
+    */
+    res.send('HOME PAGE');
+});
+
+router.get('/students', auth.isAdminLoggedIn, (req, res) => {
+    /*
+    Table of students
+    */
+    res.send('HOME PAGE');
+});
+
+router.get('/attendance', auth.isAdminLoggedIn, (req, res) => {
+    /*
+    Date option
+    List of users
+    Save button
+    */
+    res.send('HOME PAGE');
+});
 
 router.get('*', (_req, res) => {
     res.redirect('/')
